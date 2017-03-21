63f6fa2c65638ea195d76187f81b0672a162a971
Justin Wang <justin@MacBookPro-5C969D721D17-2.local>
2017-03-20 22:09:24 -0400

    location graph

---
+++ WhatsBumpin/Classes/View Controllers/LocationGraphViewController.h
@@ -0,0 +1,16 @@
+//
+//  LocationGraphViewController.h
+//  WhatsBumpin
+//
+//  Created by Justin Wang on 3/20/17.
+//  Copyright © 2017 Bump Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Location.h"
+
+@interface LocationGraphViewController : UIViewController
+
+@property (strong, nonatomic) Location *location;
+
+@end
--- WhatsBumpin/Classes/View Controllers/LocationInformationViewController.m
+++ WhatsBumpin/Classes/View Controllers/LocationInformationViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LocationInformationViewController.h"
+#import "LocationGraphViewController.h"
 
 @interface LocationInformationViewController ()
 
@@ -40,14 +41,11 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if ([[segue identifier] isEqualToString:@"LocationGraph"]) {
+        ((LocationGraphViewController *)[segue destinationViewController]).location = self.location;
+    }
 }
-*/
 
 @end
