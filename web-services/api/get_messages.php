783091cf593ee0cd94256d7c1ee48edc04dff366
Jesse Cavendish <jmc242@case.edu>
2017-03-26 21:14:22 -0400

    Added insert bump query

--- web-services/api/get_messages.php
+++ web-services/api/get_messages.php
@@ -3,20 +3,23 @@ $response = array();
  
 if($_SERVER['REQUEST_METHOD']=='POST'){
 
-    $place_id = $_POST['place_id'];
+    $location_id = $_POST['location_id'];
  
     require_once '../includes/dboperation.php';
 
     $db = new DbOperation();
-    $messages = $db->get_messages($place_id);
+    $messages = $db->get_messages($location_id);
     if(!empty($messages)) {
-        $response = $messages;
+        $response['error'] = false;
+        $response = $response + $messages;
+        
     } else { 
-        $response = false;
+        $response['error'] = true;
+        $response['message'] = 'No messages for this location';       
     }
 } else {
-    $response['error']=true;
-    $response['message']=$_SERVER['REQUEST_METHOD'];
+    $response['error'] = true;
+    $response['message'] = 'You are not authorized';
 }
 echo json_encode($response);
 
--- web-services/includes/dboperation.php
+++ web-services/includes/dboperation.php
@@ -5,19 +5,20 @@ class DbOperation{
     function __construct() {
         require_once dirname(__FILE__) . '/config.php';
         require_once dirname(__FILE__) . '/dbconnect.php';
-        
+        date_default_timezone_set('America/New_York');
         $db = new DbConnect();
         $this->conn = $db->connect();
     }
  
-    public function get_messages($location_id) {
-        $query = "SELECT * FROM message WHERE location_id = '" . $location_id . "'";
+    public function get_messages($location_id_p) {
+        $query = "SELECT * FROM message WHERE location_id = ?";
         $stmt = $this->conn->prepare($query);
+        $stmt->bind_param('i', $location_id_p);
         $stmt->execute();
-        $stmt->bind_result($id, $location_id, $user_id, $message_field, $time_stamp);
+        $stmt->bind_result($id, $location_id_r, $user_id, $message_field, $time_stamp);
 
         while($stmt->fetch()) {
-            $messages[$id]['location_id'] = $location_id;
+            $messages[$id]['location_id'] = $location_id_r;
             $messages[$id]['user_id'] = $user_id;
             $messages[$id]['message_field'] = $message_field;
             $messages[$id]['time_stamp'] = $time_stamp;
@@ -26,5 +27,18 @@ class DbOperation{
         return $messages;
     }
 
+    public function insert_bump($location_id, $user_id) {
+        $query = "INSERT INTO bump(location_id, user_id, time_stamp) VALUES(?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        $time_stamp = date("Y-m-d H:i:s");
+        $stmt->bind_param('iis', $location_id, $user_id, $time_stamp);
+        $result = $stmt->execute();
+        $stmt->close();
+        if($result) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
 }
 
