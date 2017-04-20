8d4afc19cc0bf14d022f519ebfbfdc825bb2daf8
Elle Zadina <gaz16@case.edu>
2017-04-19 23:34:39 -0400

    Post request for bumps per day of week for graphs

--- WhatsBumpin/Classes/View Controllers/LocationGraphViewController.m
+++ WhatsBumpin/Classes/View Controllers/LocationGraphViewController.m
@@ -22,4 +22,22 @@
     [self setTitle:[NSString stringWithFormat: @"%@ Data", self.location.name]];
 }
 
+
+-(void)loadBumpsPerDay {
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://52.14.0.153/api/get_bumps_by_day_of_week.php"]];
+    [request setHTTPMethod:@"POST"];
+    
+    NSString *post = [[NSString alloc] initWithFormat:@"location_id=%@&submit=", @"ChIJM5WTlIT7MIgRZXbXABw3OQw"]; ///change string to location_id; hardcoded for Jolly
+    [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest: request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSString *dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"data %@", dataString);
+    }];
+    
+    [task resume];
+}
+
 @end
