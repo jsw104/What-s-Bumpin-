ace623dc471775138b870125b7eb0325da67c67a
Elle Zadina <gaz16@case.edu>
2017-04-24 15:55:26 -0400

    pull info dynamically for graph

--- WhatsBumpin/Classes/View Controllers/LocationGraphViewController.m
+++ WhatsBumpin/Classes/View Controllers/LocationGraphViewController.m
@@ -236,7 +236,7 @@ UIActivityIndicatorView *activity;
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"http://52.14.0.153/api/get_bumps_by_location_and_day_of_week.php"]];
     [request setHTTPMethod:@"POST"];
     
-    NSString *post = [[NSString alloc] initWithFormat:@"location_id=%@&submit=", @"ChIJM5WTlIT7MIgRZXbXABw3OQw"]; ///change string to location_id; hardcoded for Jolly
+    NSString *post = [[NSString alloc] initWithFormat:@"location_id=%@&submit=", _location.googlePlacesID]; ///change string to location_id; hardcoded for Jolly
     [request setHTTPBody:[post dataUsingEncoding:NSUTF8StringEncoding]];
     
     NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithRequest: request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
